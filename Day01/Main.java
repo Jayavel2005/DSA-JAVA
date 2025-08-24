00d90e1d311559116f4f4a32600b86d566c89dbb
Jayavel <jayavel1520@gmail.com>
2025-08-24 20:13:08 +0530

    Day -1

---
+++ Day01/Main.java
@@ -0,0 +1,22 @@
+// package Day01;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(input.nextInt());
+        
+        // int number = 5;
+        // float marks = 98.5f;
+        // double floatingPointNumber = 77777.55;
+        // long longNumber = 555555555555555L;
+        // char character = 'A';
+        // boolean check = true;
+
+
+        System.out.println(input.nextFloat());
+    }
+}
