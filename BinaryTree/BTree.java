d6d23c30c40920e8b17e4c8fd356043c1b46a3bc
Jayavel <jayavel1520@gmail.com>
2025-12-18 15:13:16 +0530

    feat(patterns): add pattern printing classes for various shapes

--- BinaryTree/BTree.java
+++ BinaryTree/BTree.java
@@ -1,6 +1,7 @@
 // package BinaryTree;
 
 import java.util.Queue;
+import java.util.Stack;
 import java.util.LinkedList;
 
 class Node {
@@ -55,6 +56,73 @@ class BinaryTreeImp {
         inOrder(node.right);
 
     }
+
+    public void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder(Node node) {
+        if (node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+    }
+
+    public void iterativeInorder(Node node) {
+        Node curr = node;
+        Stack<Node> stack = new Stack<>();
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            System.out.print(curr.data + " ");
+            curr = curr.right;
+        }
+    }
+
+    public void iterativePreorder(Node node) {
+        Node curr = node;
+        Stack<Node> stack = new Stack<>();
+        stack.push(curr);
+        while (!stack.isEmpty()) {
+            curr = stack.pop();
+            System.out.println(curr.data + " ");
+
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+        }
+    }
+
+    public void levelOrder(Node node) {
+        Node curr = node;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(curr);
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.println(current.data + " ");
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
 }
 
 public class BTree {
@@ -68,6 +136,10 @@ public class BTree {
             tree.createTree(data);
         }
 
-        tree.inOrder(tree.root);
+        // tree.inOrder(tree.root);
+        // tree.iterativeInorder(tree.root);
+        // tree.iterativePreorder(tree.root);
+        // tree.levelOrder(tree.root);
+        tree.postOrder(tree.root);
     }
 }
---
+++ patterns/Pattern6.java
@@ -0,0 +1,25 @@
+// package patterns;
+
+import java.util.Scanner;
+
+public class Pattern6 {
+
+    public static void main(String[] args) {
+        /*
+         * 1
+         * 2 2
+         * 3 3 3
+         * 4 4 4 4
+         * 5 5 5 5 5
+         */
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
