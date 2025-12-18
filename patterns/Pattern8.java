6c9fd7c4ae3d7255206e3ccc77430a4179b4ddeb
Jayavel <jayavel1520@gmail.com>
2025-12-18 15:24:06 +0530

    feat(patterns): add Pattern7 and Pattern8 classes for number printing patterns

--- patterns/Pattern8.java
+++ patterns/Pattern8.java
@@ -2,22 +2,24 @@
 
 import java.util.Scanner;
 
-public class Pattern7 {
+public class Pattern8 {
     /*
-     * 5 4 3 2 1
-     * 4 3 2 1
-     * 3 2 1
-     * 2 1
      * 1
+     * 2 1
+     * 3 2 1
+     * 4 3 2 1
+     * 5 4 3 2 1
      */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+
         for (int i = 1; i <= n; i++) {
-            for (int j = n + 1 - i; j >= 1; j--) {
+            for (int j = i; j >= 1; j--) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
+
     }
 }
---
+++ patterns/Pattern9.java
@@ -0,0 +1,8 @@
+// package patterns;
+
+public class Pattern9 {
+
+    public static void main(String[] args) {
+        
+    }
+}
